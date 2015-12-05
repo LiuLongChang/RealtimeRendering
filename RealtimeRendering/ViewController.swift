af5595c6213aaede60aaada4a80ab08d2b75aa4c
Llc <1040719208@qq.com>
2015-12-05 22:59:03 +0800

    Initial Commit

---
+++ RealtimeRendering/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealtimeRendering
+//
+//  Created by 刘隆昌 on 15/12/5.
+//  Copyright © 2015年 刘隆昌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
